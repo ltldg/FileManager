69653c03ecd64c05aae4d924ac3dd78f35b840b7
ltldg <6061031@gmail.com>
2024-02-05 18:42:00 +0100

    feat:add hash

--- hash/hash.js
+++ hash/hash.js
@@ -7,9 +7,16 @@ export const hash= async (input) => {
 
     const fileName = command.trim().split('hash ')[1];
 
-    const hash = crypto.createHash('sha256');
-    const stream = fs.createReadStream(the_path);
+    const hash = createHash('sha256');
+    const stream = fs.createReadStream(filePath);
 
+    stream.on('data', (data) => {
+        hash.update(data);
+    });
 
+    stream.on('end', () => {
+        const result = hash.digest('hex');
+        console.log(result);
+    });
 
 };
